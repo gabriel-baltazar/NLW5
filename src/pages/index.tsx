a008117448c7086d77a4dae1af1bd51b71ffee9f
Gabriel Baltazar <gabrielaltazar@hotmail.com>
2021-04-23 17:32:10 -0300

    Json server com static side generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-export default function Home() {
+// SPA
+// SSR
+// SSG
+
+export default function Home(props) {
+
   return (
+    <div>
     <h1>index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+// SSR
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return{
+    props:{
+      episodes: data,
+    },
+    //QUANTO EM QUANTO TEMPO SERA FEITO NOVA CHAMADA PARA API
+    revalidate: 60 * 60 * 8,
+  }
 }
